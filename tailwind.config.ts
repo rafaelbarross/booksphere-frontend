5c28096a3719c1e8741735584dc70a33f7acc02b
Rafael Barros <rsb8@aluno.ifal.edu.br>
2023-11-15 22:59:35 -0300

    feat: Altera o padrao das telas do talwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,43 @@
 import type { Config } from 'tailwindcss'
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+module.exports = withMT({
   content: [
+    // './pages/**/*.{ts,tsx}', './public/**/*.html', './node_modules/flowbite-react/**/*.js',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+      boxShadow: {
+        '3xl': '0px 0px 18px 0px rgba(0, 0, 0, 0.25)',
+      }
     },
+
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+    }
   },
-  plugins: [],
-}
-export default config
+  // plugins: [require('flowbite/plugin')],
+})
+// export default config
