b6acc7b49e6165ae18119e31b01164a6e9182c0a
Rafael Barros <rsb8@aluno.ifal.edu.br>
2023-11-15 23:02:38 -0300

    feat: Cria component de banner

---
+++ src/app/components/banner/banner.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Typography } from "@material-tailwind/react";
+
+export default function Banner() {
+
+    return (
+        <div className='mb-40 drop-shadow-xl'>
+        <Typography variant="h3" color="blue-gray" className='mb-8'>
+        Todos os gêneros
+        </Typography>
+
+        <div className=" cursor-pointer">
+          <img className="h-40 sm:h-full" src="./banner.png" alt="" />
+        </div>
+      </div>
+    );
+}
